7bbe8185d0ab3de503ec5950a04abc9bf69d3a5e
Davide Pedron <davide.pedron@thread.solutions>
2017-12-12 23:01:15 +0100

    Solve part one of day 4

---
+++ python/06-passphrase.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+print("Start...")
+
+result = 0
+import csv
+with open('code06') as csvfile:
+    file_to_be_check = csv.reader(csvfile, delimiter=' ')
+    row_number = 0
+    for row in file_to_be_check:
+        row_is_valid = True
+        i = 0
+        while i <= len(row)-1:
+            word_to_check = row[i]
+            k = i+1
+            while k <= len(row)-1:
+                if word_to_check == row[k]:
+                    row_is_valid = False
+                k += 1
+            i += 1
+        if row_is_valid:
+            result += 1
+print("...End")
+print(result)
