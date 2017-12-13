a1989e9f2c366c4bb599740c15bd1a79ee05965e
Davide Pedron <davide_pedron@yahoo.com>
2017-12-13 22:45:43 +0100

    Solve part two of day 5

---
+++ python/09-maze.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+print("Start...")
+
+steps = 0
+
+lines = open('code08').readlines()
+pointer = 0
+while pointer < len(lines):
+    jump_distance = int(lines[pointer])
+    if jump_distance >= 3:
+        lines[pointer] = int(lines[pointer]) - 1
+    else:
+        lines[pointer] = int(lines[pointer]) + 1
+    steps += 1
+    pointer += jump_distance
+
+print("...End")
+print(steps)
